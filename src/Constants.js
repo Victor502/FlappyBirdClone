aa2310266271f9f8a4e209038452e65c81109c77
Victor502 <victorahorton@gmail.com>
2019-12-12 22:29:13 -0500

    working version, that keeps score

---
+++ src/Constants.js
@@ -0,0 +1,10 @@
+import {Dimensions} from 'react-native';
+
+export default Constants = {
+  MAX_WIDTH: Dimensions.get('screen').width,
+  MAX_HEIGHT: Dimensions.get('screen').height,
+  GAP_SIZE: 320, // gap between the two parts of the pipe
+  PIPE_WIDTH: 100, // width of the pipe
+  BIRD_WIDTH: 50,
+  BIRD_HEIGHT: 41,
+};
